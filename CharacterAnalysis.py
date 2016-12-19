d121d9c77199cc43ec77f72458417af7ce94f18a
ChimdinduDenalexOrakwue <denalexorakwue1@gmail.com>
2016-12-19 01:33:42 -0600

    update

--- CharacterAnalysis.py
+++ CharacterAnalysis.py
@@ -1,10 +1,7 @@
 from TextParser import TextParser as tp
-import matplotlib
 
-
-#parser = tp(lim=1,labels=True)
 parser = tp(min_freq = 40, labels = True)
-#parser = tp(lim=100,labels=True)
+
 parser.print_characters()
 parser.read_file()
 parser.print_graph()
